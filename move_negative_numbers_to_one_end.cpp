11abc279eac9bfa8b131a613b485137233a805d7
Prince Pandey <princepandey2784@gmail.com>
2022-05-02 08:51:10 +0530

    basic

--- move_negative_numbers_to_one_end.cpp
+++ move_negative_numbers_to_one_end.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -19,3 +19,36 @@ int main()
     cout<<x<<" ";
     return 0;
 }
+
+    //time complexity o(n) and space complexity o(n)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // move all negaive elements to one end
+    int arr[]={-1,5,6,-5,-5,0,-6,6,-9,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    //method two
+    //two pointer approach
+    
+    // it works if order is not maintained
+    int l=0,r=n-1;
+    while(l<r)
+    {
+        if(arr[l]>=0)
+        l++;
+        if(arr[r]<0)
+        r--;
+        if((l<r) && arr[l]<0 && arr[r]>=0)
+        {
+            swap(arr[l],arr[r]);
+            l++;
+        
+        }
+    }
+    for(auto x:arr)
+    cout<<x<<" ";
+    return 0;
+}
+//time complexity o(n) and space complexity o(1)
